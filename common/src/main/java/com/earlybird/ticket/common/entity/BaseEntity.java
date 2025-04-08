45ba516e6e6f5d68c7daec46ae4601eb36eeec8c
cnxw4570123 <cnxw4570123@gmail.com>
2025-04-08 19:26:05 +0900

    [ET-39] fix(user) : User 엔티티 상속 문제 해결

--- common/src/main/java/com/earlybird/ticket/common/entity/BaseEntity.java
+++ common/src/main/java/com/earlybird/ticket/common/entity/BaseEntity.java
@@ -3,7 +3,10 @@ package com.earlybird.ticket.common.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -14,7 +17,9 @@ import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
+@SuperBuilder
 @EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class BaseEntity {
 
     @CreatedBy
