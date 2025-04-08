b852083420e883ddd64100f8b87e0d4dba9c4451
Sunro1994 <gwangjulsr@gmail.com>
2025-04-08 15:39:09 +0900

    [ET-25] feat(Auth) : SecurityConfig Stateless 설정

--- auth/src/main/java/com/earlybird/ticket/auth/common/config/SecurityConfig.java
+++ auth/src/main/java/com/earlybird/ticket/auth/common/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,6 +19,7 @@ public class SecurityConfig {
         http.formLogin(AbstractHttpConfigurer::disable);
         http.logout(AbstractHttpConfigurer::disable);
         http.cors(AbstractHttpConfigurer::disable);
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/external/**")
                                                .permitAll()
                                                .anyRequest()
