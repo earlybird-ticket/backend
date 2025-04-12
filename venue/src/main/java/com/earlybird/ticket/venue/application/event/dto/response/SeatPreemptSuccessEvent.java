c12b97f9418b631755f3aed295a1814d1a9265cd
ggumi <tndus50843@gmail.com>
2025-04-12 23:40:28 +0900

    [ET-80] feat(seat) : Seat Success Event에 Passport 추가 (#125)

--- venue/src/main/java/com/earlybird/ticket/venue/application/event/dto/response/SeatPreemptSuccessEvent.java
+++ venue/src/main/java/com/earlybird/ticket/venue/application/event/dto/response/SeatPreemptSuccessEvent.java
@@ -1,11 +1,15 @@
 package com.earlybird.ticket.venue.application.event.dto.response;
 
 import com.earlybird.ticket.common.entity.EventPayload;
+import com.earlybird.ticket.common.entity.PassportDto;
 import lombok.Builder;
 
 import java.util.List;
 import java.util.UUID;
 
 @Builder
-public record SeatPreemptSuccessEvent(List<UUID> seatInstanceIdList) implements EventPayload {
+public record SeatPreemptSuccessEvent(
+        PassportDto passportDto,
+        List<UUID> seatInstanceIdList
+) implements EventPayload {
 }
