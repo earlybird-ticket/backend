d360ceef2dbc1bfd40bcf900e4ecaad59d206ca7
ggumi <tndus50843@gmail.com>
2025-04-12 23:07:12 +0900

    [ET-59] feat(venue) : Fail Event에 PassPort 추가 (#114)

--- venue/src/main/java/com/earlybird/ticket/venue/application/event/dto/response/SeatReturnFailEvent.java
+++ venue/src/main/java/com/earlybird/ticket/venue/application/event/dto/response/SeatReturnFailEvent.java
@@ -1,6 +1,7 @@
 package com.earlybird.ticket.venue.application.event.dto.response;
 
 import com.earlybird.ticket.common.entity.EventPayload;
+import com.earlybird.ticket.common.entity.PassportDto;
 import lombok.Builder;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.UUID;
 
 @Builder
 public record SeatReturnFailEvent(
+        PassportDto passportDto,
         List<UUID> seatInstanceIdList,
         String code
 ) implements EventPayload {
