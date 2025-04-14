37e30c9f6f70682f84fd8a4c54f3a67c03077cea
adlaylatte <dgs2288@naver.com>
2025-04-15 03:15:50 +0900

    [ET-98] feat(concert): Concert Outbox 구현 #180
    - Kafka 의존성 추가
    - Kafka Producer Config 설정
    - Event class 생성
    - Outbox 발행로직 구현
    - Event DTO 생성

---
+++ concert/src/main/java/com/earlybird/ticket/concert/application/event/EventType.java
@@ -0,0 +1,33 @@
+package com.earlybird.ticket.concert.application.event;
+
+import com.earlybird.ticket.common.entity.EventPayload;
+import com.earlybird.ticket.concert.application.event.dto.ConcertCreateSuccessEvent;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@RequiredArgsConstructor
+public enum EventType {
+
+    CONCERT_CREATE_SUCCESS(ConcertCreateSuccessEvent.class, Topic.CONCERT_TO_VENUE);
+
+    private final Class<? extends EventPayload> payloadClass;
+    private final String topic;
+
+    public static EventType from(String type) {
+        try {
+            return valueOf(type);
+        } catch (Exception e) {
+            log.error("type={} is invalid", type, e);
+            return null;
+        }
+    }
+
+    private static class Topic {
+
+        public static final String CONCERT_TO_VENUE = "ConcertToVenue";
+
+    }
+}
