92e47c06a382d3c72db7ecd7d5376835cfe9898d
cnxw4570123 <cnxw4570123@gmail.com>
2025-04-16 09:30:44 +0900

    [ET-128] refactor(payment) : 카프카 브로커 변수로 변경 + Outbox 정리

--- common/src/main/java/com/earlybird/ticket/common/entity/OutboxRepository.java
+++
@@ -1,7 +0,0 @@
-package com.earlybird.ticket.common.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OutboxRepository extends JpaRepository<Outbox, Long> {
-
-}
