b4ff590bc930a46dac2e7189ed10f18d2eba2f48
Sunro1994 <gwangjulsr@gmail.com>
2025-04-16 12:05:51 +0900

    [ET-131] refactor(concert): Enum 에 @Enumerated(String) 속성 추가

--- concert/src/main/java/com/earlybird/ticket/concert/domain/SeatInstanceInfo.java
+++ concert/src/main/java/com/earlybird/ticket/concert/domain/SeatInstanceInfo.java
@@ -3,20 +3,14 @@ package com.earlybird.ticket.concert.domain;
 import com.earlybird.ticket.common.entity.BaseEntity;
 import com.earlybird.ticket.concert.domain.constant.Grade;
 import com.earlybird.ticket.concert.domain.constant.Section;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import java.util.UUID;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLRestriction;
+
+import java.util.UUID;
 
 @Getter
 @Entity
@@ -29,6 +23,7 @@ public class SeatInstanceInfo extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID seatInstanceInfoId;
     private Section section;
+    @Enumerated(EnumType.STRING)
     private Grade grade;
     private Integer price;
 
@@ -37,7 +32,10 @@ public class SeatInstanceInfo extends BaseEntity {
     private Concert concert;
 
     @Builder
-    public SeatInstanceInfo(Section section, Grade grade, Integer price, Concert concert) {
+    public SeatInstanceInfo(Section section,
+                            Grade grade,
+                            Integer price,
+                            Concert concert) {
         this.section = section;
         this.grade = grade;
         this.price = price;
