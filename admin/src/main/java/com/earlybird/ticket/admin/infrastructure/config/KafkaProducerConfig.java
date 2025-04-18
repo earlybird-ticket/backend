4ab154d1e3eb2131d515b661c5d1dbbc49c333a3
ggumi <tndus50843@gmail.com>
2025-04-18 19:06:39 +0900

    [ET-146] refactor(admin) : StringSerializer로 변경 (#243)

--- admin/src/main/java/com/earlybird/ticket/admin/infrastructure/config/KafkaProducerConfig.java
+++ admin/src/main/java/com/earlybird/ticket/admin/infrastructure/config/KafkaProducerConfig.java
@@ -29,7 +29,7 @@ public class KafkaProducerConfig {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         configProps.put(
                 ProducerConfig.INTERCEPTOR_CLASSES_CONFIG,
