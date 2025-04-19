177e2e425dbb0ee455dbe3af81e92ef8ffb7f993
Sunro1994 <gwangjulsr@gmail.com>
2025-04-19 15:36:05 +0900

    [ET-152] refactor(concert) : concert 회차 정보 엔티티 생성 로직 수정, 발행시 직렬화 String으로 수정

--- concert/src/main/java/com/earlybird/ticket/concert/infrastructure/KafkaProducerConfig.java
+++ concert/src/main/java/com/earlybird/ticket/concert/infrastructure/KafkaProducerConfig.java
@@ -1,7 +1,5 @@
 package com.earlybird.ticket.concert.infrastructure;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,11 +7,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @EnableAsync
 @Configuration
@@ -26,17 +26,28 @@ public class KafkaProducerConfig {
     @Bean
     public KafkaTemplate<String, String> messageRelayKafkaTemplate() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        configProps.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
-        configProps.put(ProducerConfig.ACKS_CONFIG, "all");
-        configProps.put(ProducerConfig.RETRIES_CONFIG, 3);
-        configProps.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 5);
-        configProps.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
-        configProps.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-        configProps.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
-        configProps.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "gzip");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                        bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                        StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                        StringSerializer.class);
+        configProps.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG,
+                        true);
+        configProps.put(ProducerConfig.ACKS_CONFIG,
+                        "all");
+        configProps.put(ProducerConfig.RETRIES_CONFIG,
+                        3);
+        configProps.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION,
+                        5);
+        configProps.put(ProducerConfig.BATCH_SIZE_CONFIG,
+                        16384);
+        configProps.put(ProducerConfig.LINGER_MS_CONFIG,
+                        1);
+        configProps.put(ProducerConfig.BUFFER_MEMORY_CONFIG,
+                        33554432);
+        configProps.put(ProducerConfig.COMPRESSION_TYPE_CONFIG,
+                        "gzip");
         return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(configProps));
     }
 
