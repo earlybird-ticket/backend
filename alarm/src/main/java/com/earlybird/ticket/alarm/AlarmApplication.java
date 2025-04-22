dc2028b69858d2923e2855e00d993ee2a9927545
Sunro1994 <gwangjulsr@gmail.com>
2025-04-22 10:11:05 +0900

    [ET-169] chore(alarm) : 모듈 생성

---
+++ alarm/src/main/java/com/earlybird/ticket/alarm/AlarmApplication.java
@@ -0,0 +1,16 @@
+package com.earlybird.ticket.alarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AlarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlarmApplication.class,
+                              args);
+    }
+
+}
