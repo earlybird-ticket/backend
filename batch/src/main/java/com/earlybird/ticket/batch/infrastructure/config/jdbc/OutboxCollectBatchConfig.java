cd669a982ab7d088db9122d720cf7e4281df26cd
cnxw4570123 <cnxw4570123@gmail.com>
2025-04-25 16:14:46 +0900

    [ET-190] feat(batch) : 각 서비스 스텝 Job에 연결

--- batch/src/main/java/com/earlybird/ticket/batch/infrastructure/config/jdbc/OutboxCollectBatchConfig.java
+++ batch/src/main/java/com/earlybird/ticket/batch/infrastructure/config/jdbc/OutboxCollectBatchConfig.java
@@ -2,30 +2,16 @@ package com.earlybird.ticket.batch.infrastructure.config.jdbc;
 
 import com.earlybird.ticket.batch.domain.entity.Outbox;
 import com.earlybird.ticket.batch.infrastructure.dto.response.OutboxMessage;
-import java.util.Map;
-import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.database.JdbcPagingItemReader;
-import org.springframework.batch.item.database.Order;
-import org.springframework.batch.item.database.PagingQueryProvider;
-import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
-import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
-import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
-import org.springframework.batch.item.support.CompositeItemWriter;
-import org.springframework.batch.item.support.builder.CompositeItemWriterBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.DataClassRowMapper;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @Slf4j
 @Configuration
@@ -49,20 +35,25 @@ public class OutboxCollectBatchConfig {
             .build();
     }
 
-    /*
-     * TODO : admin, concert, coupon, reservation, venue 추가
-     *  1. ItemReader 빈 추가
-     *  2. QueryProvider 추가
-     *  3. ItemWriter 빈 추가
-     *  4. 각 서비스 step으로 빈 추가
-     *  5. Job에 각 서비스 Step 연결
-     */
     @Bean
-    public Job collectOutboxJob(Step collectPaymentOutboxStep, JobRepository jobRepository) {
+    public Job collectOutboxJob(
+        @Qualifier("collectPaymentOutboxStep") Step collectPaymentOutboxStep,
+        @Qualifier("collectCouponOutboxStep") Step collectCouponOutboxStep,
+        @Qualifier("collectConcertOutboxStep") Step collectConcertOutboxStep,
+        @Qualifier("collectVenueOutboxStep") Step collectVenueOutboxStep,
+        @Qualifier("collectAdminOutboxStep") Step collectAdminOutboxStep,
+        @Qualifier("collectReservationOutboxStep") Step collectReservationOutboxStep,
+        JobRepository jobRepository
+    ) {
         // TODO : step으로 admin, concert, coupon, reservation, venue 추가
         return new JobBuilder(OUTBOX_COLLECT_JOB, jobRepository)
             .incrementer(new RunIdIncrementer())
             .start(collectPaymentOutboxStep)
+            .next(collectCouponOutboxStep)
+            .next(collectConcertOutboxStep)
+            .next(collectVenueOutboxStep)
+            .next(collectAdminOutboxStep)
+            .next(collectReservationOutboxStep)
             .build();
     }
 }
