33fac8b0ce2142ed8aee74d88a12d71864959851
cnxw4570123 <cnxw4570123@gmail.com>
2026-03-30 01:08:05 +0900

    feat(venue): 부하 테스트용 warmup 트리거 엔드포인트 추가
    - TestWarmupController 추가

---
+++ venue/src/main/java/com/earlybird/ticket/venue/presentation/controller/TestWarmupController.java
@@ -0,0 +1,29 @@
+package com.earlybird.ticket.venue.presentation.controller;
+
+import com.earlybird.ticket.venue.application.dto.request.WarmupSeatCommand;
+import com.earlybird.ticket.venue.application.service.WarmupSeatOrchestrator;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/internal/test/warmup")
+@Profile("loadtest")
+@RequiredArgsConstructor
+public class TestWarmupController {
+
+    private final WarmupSeatOrchestrator orchestrator;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void warmup(@RequestBody List<WarmupSeatCommand> commands) {
+        orchestrator.warmup(commands);
+    }
+
+}
